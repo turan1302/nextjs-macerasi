42f02761bb8860d6ee0f87d1c7e8af275287a384
turan1302 <printwordex@gmail.com>
2025-03-07 09:18:14 +0300

    prop types konusuna deginildi

---
+++ bolum_38/src/app/admin/users/page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Sidebar from "@/components/back/common/Sidebar";
+import Navbar from "@/components/back/common/Navbar";
+import Footer from "@/components/back/common/Footer";
+import Report from "@/components/back/report";
+
+const UsersPage = () => {
+    return (
+        <>
+            <Sidebar/>
+
+            <div id="content-wrapper" className="d-flex flex-column">
+
+                <div id="content">
+
+                    <Navbar/>
+
+                    <div className="container-fluid">
+                        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                            <h1 className="h3 mb-0 text-gray-800">Kullanıcılar</h1>
+                        </div>
+
+                        <Report aylik={"240.000"} haftalik={"50.000"} bekleyen_gorev={50} onaylanan_istek={20}/>
+
+                    </div>
+
+                </div>
+
+                <Footer/>
+            </div>
+        </>
+    )
+}
+
+export default UsersPage
